103426181284dabb621b2d9eff8f70cc6659e370
DmitryMartyushev <martyushev14@yandex.ru>
2020-05-05 02:20:06 +0300

    test

--- BytePack/BytePack.cpp
+++ BytePack/BytePack.cpp
@@ -32,7 +32,18 @@ void BytePack::sendNextBoard() {
     Serial.println(_secondByte, BIN);
     _flagSend = 0;
   }
+}
 
-
+int BytePack::numberChannel(byte secondByte) {
+  _numbCh = _secondByte >> 4;
+  _numbCh &= ~((1 << 7) | (1 << 6) | (1 << 5) | (1 << 4) | (1 << 3)); //выключаем 7, 6, 5, 4, 3 биты
+  return _numbCh;
 
 }
+
+int BytePack::numberSwitch(byte secondByte){
+  _numbSw = _secondByte;
+  _numbSw &= ~((1<<7)|(1<<6)|(1<<5)|(1<<4)); // выключаем 7, 6, 5, 4 биты
+  return _numbSw;
+  
+  }
--- BytePack/BytePack.h
+++ BytePack/BytePack.h
@@ -8,14 +8,16 @@ class BytePack {
     bool readPack(); // чтение данных
     void sendNextBoard(); // отправить на след. плату
     bool addresCheck(byte addresBoard); // проверка адреса платы
-    byte numberSwitch(byte secondByte); // номер ключа
-    byte numberChannel(byte secondByte); // номер канала
+    int numberSwitch(byte secondByte); // номер ключа
+    int numberChannel(byte secondByte); // номер канала
 
   private:
     byte _firstByte;
     byte _secondByte;
     bool _flagSend;
     byte _addresPack;
+    byte _numbCh;
+    byte _numbSw;
 };
 
 #endif
